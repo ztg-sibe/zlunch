5a4844124c941ae131e1d029cde4fd5d701adf7b
Simon Beckstein <simon.beckstein@gmail.com>
2019-05-08 16:51:54 +0200

    fixed post

--- src/main/kotlin/com/zuehlke/camp/zlunch/LunchController.kt
+++ src/main/kotlin/com/zuehlke/camp/zlunch/LunchController.kt
@@ -3,6 +3,7 @@ package com.zuehlke.camp.zlunch
 import com.zuehlke.camp.zlunch.entity.Lunch
 import com.zuehlke.camp.zlunch.services.LunchService
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
@@ -11,7 +12,7 @@ class LunchController(val lunchService: LunchService) {
 
 
     @PostMapping("/lunches")
-    fun createLunch(lunch: Mono<Lunch>): Mono<Lunch> {
+    fun createLunch(@RequestBody lunch: Mono<Lunch>): Mono<Lunch> {
         return lunchService.createLunch(lunch);
     }
 }
