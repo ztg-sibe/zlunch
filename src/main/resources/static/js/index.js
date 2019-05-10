55b8a975d891cc172019306c7adabeefaa3c67e8
Scherer <konstantin.scherer@zuehlke.com>
2019-05-10 09:02:20 +0200

    get participants for lunch, notify about new lunch by alert

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,19 @@
 var eventUrl = "http://localhost:8080/lunchEvents";
+
+Vue.component('lunch-participation-list', {
+    props: ['lunch'],
+    data: function () {
+        return {message: []};
+    },
+    template: '<div><div v-for="item in message">{{item.user.name}}</div></div>',
+    created: function () {
+        this.$http.get("/lunches/" + this.lunch + "/participations")
+            .then(function (data) {
+                this.message = data.body;
+            });
+    }
+})
+
 new Vue({
     el: '#app',
     data: {
@@ -16,7 +31,7 @@ new Vue({
             var es = new EventSource(eventUrl);
             es.addEventListener('message', function (event) {
                 var data = JSON.parse(event.data);
-                console.log(data.lunch.name);
+                alert("New lunch arrived! Hungry? Then subscribe to:" + data.lunch.name);
             }, false);
         }
     }
